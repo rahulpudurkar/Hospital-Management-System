ddbc5fafb91a6f421a0f32e510b4315ecbb6e052
Rahul Pudurkar <rahulpudurkar68@gmail.com>
2021-05-06 16:58:01 +0530

    first commit

---
+++ package/appointment.py
@@ -0,0 +1,66 @@
+#Tushar Borole
+#Python 2.7
+
+from flask_restful import Resource, Api, request
+from package.model import conn
+import pymongo
+
+client = pymongo.MongoClient("mongodb+srv://Sanjo:sanjo_boo02@cluster0.vaait.mongodb.net/Database?retryWrites=true&w=majority")
+db = client['Database']
+col = db['appointment']
+
+
+class Appointments(Resource):
+    """This contain apis to carry out activity with all appiontments"""
+
+    def get(self):
+        """Retrive all the appointment and return in form of json"""
+
+        appointment = conn.execute("SELECT p.*,d.*,a.* from appointment a LEFT JOIN patient p ON a.pat_id = p.pat_id LEFT JOIN doctor d ON a.doc_id = d.doc_id ORDER BY appointment_date DESC").fetchall()
+        return appointment
+
+    def post(self):
+        """Create the appoitment by assiciating patient and docter with appointment date"""
+
+        appointment = request.get_json(force=True)
+        pat_id = appointment['pat_id']
+        doc_id = appointment['doc_id']
+        appointment_date = appointment['appointment_date']
+        appointment['app_id'] = conn.execute('''INSERT INTO appointment(pat_id,doc_id,appointment_date)
+            VALUES(?,?,?)''', (pat_id, doc_id,appointment_date)).lastrowid
+        dic = {"id":appointment['app_id'],"pat_id":pat_id,"doc_id":doc_id,"time": appointment_date}
+        col.insert_one(dic)
+        conn.commit()
+        return appointment
+
+
+
+class Appointment(Resource):
+    """This contain all api doing activity with single appointment"""
+
+    def get(self,id):
+        """retrive a singe appointment details by its id"""
+
+        appointment = conn.execute("SELECT * FROM appointment WHERE app_id=?",(id,)).fetchall()
+        return appointment
+
+
+    def delete(self,id):
+        """Delete teh appointment by its id"""
+
+        conn.execute("DELETE FROM appointment WHERE app_id=?",(id,))
+        myquery = { "id": id }
+        col.delete_one(myquery)
+        conn.commit()
+        return {'msg': 'sucessfully deleted'}
+
+    def put(self,id):
+        """Update the appointment details by the appointment id"""
+
+        appointment = request.get_json(force=True)
+        pat_id = appointment['pat_id']
+        doc_id = appointment['doc_id']
+        conn.execute("UPDATE appointment SET pat_id=?,doc_id=? WHERE app_id=?",
+                     (pat_id, doc_id, id))
+        conn.commit()
+        return appointment
---
+++ package/doctor.py
@@ -0,0 +1,73 @@
+#Tushar Borole
+#Python 2.7
+
+from flask_restful import Resource, Api, request
+from package.model import conn
+import pymongo
+
+
+client = pymongo.MongoClient("mongodb+srv://Sanjo:sanjo_boo02@cluster0.vaait.mongodb.net/Database?retryWrites=true&w=majority")
+db = client['Database']
+col = db['doctors']
+
+class Doctors(Resource):
+    """This contain apis to carry out activity with all doctors"""
+
+    def get(self):
+        """Retrive list of all the doctor"""
+
+        doctors = conn.execute("SELECT * FROM doctor ORDER BY doc_date DESC").fetchall()
+        return doctors
+
+
+
+    def post(self):
+        """Add the new doctor"""
+
+        doctorInput = request.get_json(force=True)
+        doc_first_name=doctorInput['doc_first_name']
+        doc_last_name = doctorInput['doc_last_name']
+        doc_ph_no = doctorInput['doc_ph_no']
+        doc_address = doctorInput['doc_address']
+        doctorInput['doc_id']=conn.execute('''INSERT INTO doctor(doc_first_name,doc_last_name,doc_ph_no,doc_address)
+            VALUES(?,?,?,?)''', (doc_first_name, doc_last_name,doc_ph_no,doc_address)).lastrowid
+        conn.commit()
+        dic = {"id":doctorInput['doc_id'],"first_name":doc_first_name,"last_name":doc_last_name,"phone":doc_ph_no,"address":doc_address}
+        col.insert_one(dic)
+        return doctorInput
+
+class Doctor(Resource):
+    """It include all the apis carrying out the activity with the single doctor"""
+
+
+    def get(self,id):
+        """get the details of the docktor by the doctor id"""
+
+        doctor = conn.execute("SELECT * FROM doctor WHERE doc_id=?",(id,)).fetchall()
+        return doctor
+
+    def delete(self, id):
+        """Delete the doctor by its id"""
+
+        conn.execute("DELETE FROM doctor WHERE doc_id=?", (id,))
+        myquery = { "id": id }
+        col.delete_one(myquery)
+        conn.commit()
+        return {'msg': 'sucessfully deleted'}
+
+    def put(self,id):
+        """Update the doctor by its id"""
+
+        doctorInput = request.get_json(force=True)
+        doc_first_name=doctorInput['doc_first_name']
+        doc_last_name = doctorInput['doc_last_name']
+        doc_ph_no = doctorInput['doc_ph_no']
+        doc_address = doctorInput['doc_address']
+        conn.execute(
+            "UPDATE doctor SET doc_first_name=?,doc_last_name=?,doc_ph_no=?,doc_address=? WHERE doc_id=?",
+            (doc_first_name, doc_last_name, doc_ph_no, doc_address, id))
+        myquery = { "id": id }
+        newvalues = { "$set": {"id":id,"first_name":doc_first_name,"last_name":doc_last_name,"phone":doc_ph_no,"address":doc_address} }
+        col.update_one(myquery, newvalues)
+        conn.commit()
+        return doctorInput
